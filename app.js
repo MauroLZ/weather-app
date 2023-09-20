6b2265c8506e443b160c10c3d4fe78d574ad7449
MauroLZ <mauro.lara@uabc.edu.mx>
2023-09-19 23:47:25 -0700

    - Fixed an error where it didn't change correctly from evening to other times

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ function updateContainerStyle(hour) {
         submitButton.classList.add('button-night');
         submitButton.classList.remove('button-morning');
         submitButton.classList.remove('button-evening');
-        submitButton.classList.remove('button-day');
+        submitButton.classList.remove('button-afternoon');
     } else if (hourNumber >= 6 && hourNumber < 12) {
         container.style.backgroundImage = 'url(/images/sunrise.jpg)'; // Morning background
         submitButton.classList.add('button-morning');
         submitButton.classList.remove('button-night');
         submitButton.classList.remove('button-evening');
-        submitButton.classList.remove('button-day');
+        submitButton.classList.remove('button-afternoon');
     } else if (hourNumber >= 12 && hourNumber < 18) {
         container.style.backgroundImage = 'url(/images/day.jpg)'; // Day background
         submitButton.classList.add('button-afternoon');
@@ -29,7 +29,7 @@ function updateContainerStyle(hour) {
         submitButton.classList.add('button-evening');
         submitButton.classList.remove('button-night');
         submitButton.classList.remove('button-morning');
-        submitButton.classList.remove('button-day');
+        submitButton.classList.remove('button-afternoon');
     }
 }
 
